b06fa655bf4f5ef9f1cb32347526a33505f0dfda
Tabreze <mkarimee@lendingclub.com>
2026-07-10 17:07:44 +0530

    Changed locator in updateemployee

--- Projects/OrangeHRM/Pages/UpdateEmployeePage.ts
+++ Projects/OrangeHRM/Pages/UpdateEmployeePage.ts
@@ -26,23 +26,27 @@ export class UpdateEmployeePage {
 
     async updateEmployee(employeeName: string) {
         const employeeRow = this.page.locator('.oxd-table-card, [role="row"]').filter({ hasText: employeeName }).first();
-
+        const maritalField = this.page.locator('.oxd-input-group').filter({ hasText: 'Marital Status' });
+        const genderField = this.page.locator('.oxd-input-group').filter({ hasText: 'Gender' });
+        const dobField = this.page.locator('.oxd-input-group').filter({ hasText: 'Date of Birth' });
+                
         await expect(employeeRow).toBeVisible();
         await this.page.locator('.oxd-table-card-cell-checkbox > .oxd-checkbox-wrapper > label > .oxd-checkbox-input > .oxd-icon').click();
         await this.page.getByRole('button').filter({ hasText: /^$/ }).nth(3).click();
         await this.page.locator('.oxd-icon.bi-caret-down-fill.oxd-select-text--arrow').first().click();
         await this.page.getByText('Indian').click();
-        await this.page.locator('div:nth-child(2) > .oxd-input-group > div:nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text--after > .oxd-icon').click();
-        await this.page.getByText('Married').click();
+        await maritalField.locator('.oxd-select-text').click();
+        await this.page.getByRole('listbox').getByText('Married', { exact: true }).click();
+        await genderField.getByText('Male', { exact: true }).locator('..').locator('.oxd-radio-input').click();
         await this.page.locator('.oxd-radio-input').first().click();
-        await this.page.locator('div:nth-child(5) > div:nth-child(2) > div > .oxd-input-group > div:nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-icon').click();
+        await dobField.locator('.oxd-date-input').click();
         await this.page.getByText('2026', { exact: true }).click();
         await this.page.getByText('1970').click();
         await this.page.getByText('5', { exact: true }).click();
         await this.page.locator('form').filter({ hasText: 'Employee Full NameEmployee' }).getByRole('button').click();
 
         await expect(
-            this.page.getByText(/Successfully Updated/i)
+            this.page.getByText(/Successfully Saved/i)
         ).toBeVisible();
 
         console.log('Employee updated successfully');
